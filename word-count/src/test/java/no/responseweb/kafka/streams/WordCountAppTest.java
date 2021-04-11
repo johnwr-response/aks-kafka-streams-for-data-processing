d9e579acb8459e7a2cb6d95459014ca35656af16
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2021-04-11 21:53:16 +0200

    Word Count Application Topology

---
+++ word-count/src/test/java/no/responseweb/kafka/streams/WordCountAppTest.java
@@ -0,0 +1,88 @@
+package no.responseweb.kafka.streams;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.apache.kafka.streams.test.ConsumerRecordFactory;
+import org.apache.kafka.streams.test.OutputVerifier;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordCountAppTest {
+    TopologyTestDriver testDriver;
+
+    StringSerializer stringSerializer = new StringSerializer();
+
+    ConsumerRecordFactory<String, String> recordFactory =
+            new ConsumerRecordFactory<>(stringSerializer, stringSerializer);
+
+
+    @BeforeEach
+    public void setUpTopologyTestDriver(){
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        WordCountApp wordCountApp = new WordCountApp();
+        Topology topology = wordCountApp.createTopology();
+        testDriver = new TopologyTestDriver(topology, config);
+    }
+
+    @AfterEach
+    public void closeTestDriver(){
+        testDriver.close();
+    }
+
+    public void pushNewInputRecord(String value){
+        testDriver.pipeInput(recordFactory.create("word-count-input", null, value));
+    }
+
+    @Test
+    void dummyTest(){
+        String dummy = "Du" + "mmy";
+        assertEquals("Dummy", dummy);
+    }
+
+    public ProducerRecord<String, Long> readOutput(){
+        return testDriver.readOutput("word-count-output", new StringDeserializer(), new LongDeserializer());
+    }
+
+    @Test
+    void makeSureCountsAreCorrect(){
+        String firstExample = "testing Kafka Streams";
+        pushNewInputRecord(firstExample);
+        OutputVerifier.compareKeyValue(readOutput(), "testing", 1L);
+        OutputVerifier.compareKeyValue(readOutput(), "kafka", 1L);
+        OutputVerifier.compareKeyValue(readOutput(), "streams", 1L);
+        assertNull(readOutput());
+
+        String secondExample = "testing Kafka again";
+        pushNewInputRecord(secondExample);
+        OutputVerifier.compareKeyValue(readOutput(), "testing", 2L);
+        OutputVerifier.compareKeyValue(readOutput(), "kafka", 2L);
+        OutputVerifier.compareKeyValue(readOutput(), "again", 1L);
+
+    }
+
+    @Test
+    void makeSureWordsBecomeLowercase(){
+        String upperCaseString = "KAFKA kafka Kafka";
+        pushNewInputRecord(upperCaseString);
+        OutputVerifier.compareKeyValue(readOutput(), "kafka", 1L);
+        OutputVerifier.compareKeyValue(readOutput(), "kafka", 2L);
+        OutputVerifier.compareKeyValue(readOutput(), "kafka", 3L);
+
+    }
+}
